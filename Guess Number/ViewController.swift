2dc8230f85c0ba4de49a0a8d7d33bb1f50523574
Tim Tseng <tim.kctseng@gmail.com>
2017-08-05 02:50:34 +0800

    2017.08.05

--- Guess Number/ViewController.swift
+++ Guess Number/ViewController.swift
@@ -9,7 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var countNumber:Int = 1
+    var maxTry:Int = 5
+    
+    @IBOutlet weak var timesNumber: UILabel!
+    @IBOutlet weak var pickedNumber: UITextField!
+    @IBOutlet weak var answerWord: UILabel!
+    @IBAction func submitNumber(_ sender: UIButton) {
+     pickedNumber.resignFirstResponder() //
+        
+        
+        
+        let upperNumber:Int = 10
+        let bottomNumber:Int = 1
+        let luckyNumber:Int = Int(arc4random_uniform(100))+1
+       
+        if Int(pickedNumber.text!) == nil {
+            answerWord.text = "Please Input Number"
+            timesNumber.text = "\(maxTry+1-countNumber) chances left"
+        
+        }
+            
+        else if Int(pickedNumber.text!)! > upperNumber  {
+            answerWord.text = " Outside the Range"
+             timesNumber.text = "\(maxTry+1-countNumber) chances left"
+        }
+        else if Int(pickedNumber.text!)! < bottomNumber {
+             answerWord.text = "Outside the Range"
+             timesNumber.text = "\(maxTry+1-countNumber) chances left"
+        }
+        else if Int(pickedNumber.text!) == luckyNumber {
+            answerWord.text = "AWESOME"
+            timesNumber.text = "Start Over Again"
+            countNumber = 1
+            }
+            
+        else {
+            
+            if countNumber < maxTry {
+                answerWord.text = "Keep Trying"
+                countNumber += 1
+                timesNumber.text = "\(maxTry+1-countNumber) chances left"
+            }
+            else {
+              timesNumber.text = "Start Over Again"
+              countNumber = 1
+            
+            }
+            }
+        
+        
+        }
+ 
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
